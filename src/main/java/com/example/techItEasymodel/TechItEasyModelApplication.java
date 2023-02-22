6a77a351f7f84ecea38754f39fe78cca896a3d1f
chaudhryhamidali <ch.hamidali@gmail.com>
2023-02-22 21:17:38 +0100

    firstcommit

---
+++ src/main/java/com/example/techItEasymodel/TechItEasyModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.techItEasymodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechItEasyModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechItEasyModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/techItEasymodel/TechItEasyModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techItEasymodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechItEasyModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
